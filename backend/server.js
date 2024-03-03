de778ef9b7eb1a7a1bd8db02c35be4c191b014cb
tofa93 <tofig.valiyev1993@gmail.com>
2024-03-03 17:55:21 +0100

    First Deployment of a Backend Express application

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ mssg: "Welcome to the app" })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port 4000")
+})
