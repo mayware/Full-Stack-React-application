e3bb3241d6b058e83112ae82371372d2521f61e4
tofa93 <tofig.valiyev1993@gmail.com>
2024-05-19 21:37:11 +0200

    added fetch request code lines

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+
+    const [products, setProducts] = useState(null)
 
     useEffect(() => {
         const fetchProducts = async () => {
             const response = await fetch('/api/shop/store')
-
             const jasonData = await response.json()
+
+            if (response.ok) {
+                setProducts(jasonData)
+            }
         }
+        fetchProducts()
     }, [])
 
     return (
         <div className="home-page">
             <div className="store-products">
-                { }
+                {products && products.map((product) => (
+                    <p key={product._id}>{product.title}</p>
+                ))}
             </div>
         </div>
     );
