116e32cca274201346d5bb912be47d125fd642ee
tofa93 <tofig.valiyev1993@gmail.com>
2024-05-20 03:21:17 +0200

    added few lines of code\

--- frontend/src/components/Sidenav.js
+++ frontend/src/components/Sidenav.js
@@ -1,7 +1,22 @@
 const Sidenav = () => {
     return (
         <aside className="sidenav">
-            Sidenav
+            <div className="sidenav-container">
+                <ul className="sidenav-items-list">
+                    <li className="sidenav-list-item">
+                        <a href="#" className="sidenav-item">Item</a>
+                    </li>
+                    <li className="sidenav-list-item">
+                        <a href="#" className="sidenav-item">Item</a>
+                    </li>
+                    <li className="sidenav-list-item">
+                        <a href="#" className="sidenav-item">Item</a>
+                    </li>
+                    <li className="sidenav-list-item">
+                        <a href="#" className="sidenav-item">Item</a>
+                    </li>
+                </ul>
+            </div>
         </aside>);
 }
 
