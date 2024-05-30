d50ec8ddd864a73702887a3e6b99926f3acba6e8
tofa93 <tofig.valiyev1993@gmail.com>
2024-05-30 23:19:05 +0200

    added few more lines of code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import StoreListCarousel from "../components/StoreListCarousel"
 
 const Home = () => {
 
@@ -17,21 +18,31 @@ const Home = () => {
     }, [])
 
     return (
-        <div className="store-products">
-            {products && products.map((product) => (
-                <div className="product-card" key={product._id}>
-                    <div className="product-card-inner">
-                        <div className="product-image-box">
-                            <img src={product.image} />
-                        </div>
-                        <div className="product-detail-box">
-                            <p>{product.title}</p>
-                            <p>{product.price}</p>
-                            <p>{product.weight}</p>
+        <div className="main-store">
+            <StoreListCarousel />
+            <div className="store-products">
+                {products && products.map((product) => (
+                    <div className="product-card" key={product._id}>
+                        <div className="product-card-inner">
+                            <div className="product-card-upper">
+                                <div className="product-image-box">
+                                    <img src={product.image} />
+                                </div>
+                            </div>
+                            <div className="product-card-lower">
+                                <div className="product-detail-box">
+                                    <p>{product.title}</p>
+                                    <p>{product.price}</p>
+                                    <p>{product.weight}</p>
+                                </div>
+                                <div className="add-to-cart-button-box">
+                                    <button className="add-to-cart-button">Add to Cart</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
